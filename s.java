b9721a763f9fd134c3f5a8da8048ee873e00b28f
Prasanna <prasannahanchinamani.15@gmail.com>
2025-07-16 11:00:06 +0530

    Update s.java

--- s.java
+++ s.java
@@ -2,5 +2,6 @@ public class s {
     public static void main(String[] args) {
         System.out.println("Hello from S.java!");
         System.out.println(" learn pr");
+        s;alkjdhg
     }
 }
